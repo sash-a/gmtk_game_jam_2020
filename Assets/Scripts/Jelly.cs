18270fcef9a946abd10549b6cd8088ee1924e361
Liron Toledo <ldoggy20@gmail.com>
2020-07-11 03:53:36 +0200

    Merge branch 'master' of https://github.com/sash-a/gmtk_game_jam_2020
    # Conflicts:
    #	Assets/Code/Map/Grid.cs
    #	Assets/Code/Player/PlayerController.cs.meta
    #	Assets/PlayerController.cs
    #	Assets/PlayerController.cs.meta
    #	Assets/Prefabs.meta
    #	Assets/Scripts/PlayerController.cs
    #	Assets/Scripts/PlayerController.cs.meta
    
    Signed-off-by: Liron Toledo <ldoggy20@gmail.com>

--- Assets/Scripts/Jelly.cs
+++ Assets/Scripts/Jelly.cs
@@ -5,34 +5,106 @@ using UnityEngine;
 public class Jelly : MonoBehaviour
 {
     private Mesh mesh;
-    public Vector3[] vertices;
+    public Vector3[] verticies;
     public int CenterPoint;
     public int verticiesCount;
 
     public List<GameObject> points;
     public GameObject toBeInstantiated;
 
-    public float speed = 10;
+    public PolygonCollider2D polyCollider;
+    public float radius= 5;
+    public int vertexNum = 32;
 
-    private void Awake()
+    void Start()
+    {
+        polyCollider = GetComponent<PolygonCollider2D>();
+        PolyMesh(radius, vertexNum);
+        MakeMeshJelly();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < verticies.Length; i++)
+        {
+            verticies[i] = points[i].transform.localPosition;
+        }
+        mesh.vertices = verticies;
+    }
+
+    void PolyMesh(float radius, int n)
+    {
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        mf.mesh = mesh;
+
+        //verticies
+        List<Vector3> verticiesList = new List<Vector3> { };
+        float x;
+        float y;
+        for (int i = 0; i < n; i++)
+        {
+            x = radius * Mathf.Sin((2 * Mathf.PI * i) / n);
+            y = radius * Mathf.Cos((2 * Mathf.PI * i) / n);
+            verticiesList.Add(new Vector3(x, y, 0f));
+        }
+        Vector3 [] verticies = verticiesList.ToArray();
+
+        //triangles
+        List<int> trianglesList = new List<int> { };
+        for (int i = 0; i < (n - 2); i++)
+        {
+            trianglesList.Add(0);
+            trianglesList.Add(i + 1);
+            trianglesList.Add(i + 2);
+        }
+        int[] triangles = trianglesList.ToArray();
+
+        //normals
+        List<Vector3> normalsList = new List<Vector3> { };
+        for (int i = 0; i < verticies.Length; i++)
+        {
+            normalsList.Add(-Vector3.forward);
+        }
+        Vector3[] normals = normalsList.ToArray();
+
+        //initialise
+        mesh.vertices = verticies;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+
+        //polyCollider
+        polyCollider.pathCount = 1;
+
+        List<Vector2> pathList = new List<Vector2> { };
+        for (int i = 0; i < n; i++)
+        {
+            pathList.Add(new Vector2(verticies[i].x, verticies[i].y));
+        }
+        Vector2[] path = pathList.ToArray();
+
+        polyCollider.SetPath(0, path);
+    }
+
+    void MakeMeshJelly()
     {
         mesh = GetComponent<MeshFilter>().mesh;
-        vertices = mesh.vertices;
-        verticiesCount = vertices.Length;
+        verticies = mesh.vertices;
+        verticiesCount = verticies.Length;
 
-        for (int i = 0; i < vertices.Length; i++)
+        for (int i = 0; i < verticies.Length; i++)
         {
-            GameObject childObject = Instantiate(toBeInstantiated, transform.position + vertices[i], Quaternion.identity) as GameObject;
+            GameObject childObject = Instantiate(toBeInstantiated, transform.position + verticies[i], Quaternion.identity) as GameObject;
             childObject.transform.parent = gameObject.transform;
             points.Add(childObject);
         }
 
-        GameObject centre = points[CenterPoint];
+        //GameObject centre = points[CenterPoint];
         for (int i = 0; i < points.Count; i++)
         {
-            if (i != CenterPoint || points[i] != centre)
-            {
-                if (i == points.Count-1)
+            //if (i != CenterPoint)
+            //{
+                if (i == points.Count - 1)
                 {
                     points[i].GetComponent<HingeJoint2D>().connectedBody = points[0].GetComponent<Rigidbody2D>();
                 }
@@ -40,22 +112,9 @@ public class Jelly : MonoBehaviour
                 {
                     points[i].GetComponent<HingeJoint2D>().connectedBody = points[i + 1].GetComponent<Rigidbody2D>();
                 }
-            }
-            else
-            {
-                points[i].GetComponent<HingeJoint2D>().connectedBody = points[i].GetComponent<Rigidbody2D>();
-            }
+           // }
         }
 
+        //points[CenterPoint].GetComponent<HingeJoint2D>().connectedBody = points[CenterPoint].GetComponent<Rigidbody2D>();
     }
-
-    void Update()
-    {
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            vertices[i] = points[i].transform.localPosition;
-        }
-        mesh.vertices = vertices;
-    }
-
 }
