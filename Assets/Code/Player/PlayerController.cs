5febe94111ae77edd789f76655ccf57b98f7e4b3
sasha <reallysasha@gmail.com>
2020-07-12 00:00:33 +0200

    tuned player movement

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor.U2D.Path;
+using UnityEngine;
 
 namespace Code.Player
 {
@@ -6,9 +8,11 @@ namespace Code.Player
     {
         public float snappyness;
         public float speed;
-        public float jumpForce;
+        public float jumpSpeed;
         public float sizeSpeedInfluence;
         public float sizeJumpInfluence;
+
+        public LayerMask onlyFloor;
 
         [HideInInspector] public bool airborn = true;
         private Vector2 velocity;  // for movement interp
@@ -27,21 +31,35 @@ namespace Code.Player
             var currVelocity = rb.velocity;
         
             // left right
-            var dir = Input.GetAxisRaw("Horizontal") * horizontalFlip;
-        
-            Vector2 targetVelocity = new Vector2(dir * speed, currVelocity.y);
-            rb.velocity = Vector2.SmoothDamp(currVelocity, targetVelocity, ref velocity, snappyness * (transform.localScale.magnitude * sizeSpeedInfluence));
-
+            var dir = (int) Input.GetAxisRaw("Horizontal") * horizontalFlip;
+            if (OnWall(dir))
+                dir = 0;
+            
+            // print(dir);
+            
+            // print(transform.localScale.x);
+            float scaleFactor = -1.5f*Mathf.Log(transform.localScale.x + 1f) + 2f;
+            Vector2 targetVelocity = new Vector2(dir * speed + scaleFactor * dir, currVelocity.y);
+            // rb.velocity = Vector2.SmoothDamp(currVelocity, targetVelocity, ref velocity, snappyness * (transform.localScale.magnitude * sizeSpeedInfluence));
+            
             // Jump
+            if (Input.GetKeyDown(KeyCode.W))
+                print(airborn);
             if (!airborn)
             {
                 if (Input.GetKeyDown(KeyCode.W))
                 {
-                    rb.AddForce(new Vector2(0f, jumpForce * (1f/transform.localScale.magnitude * sizeJumpInfluence)));
                     airborn = true;
+                    scaleFactor = -15 * Mathf.Log10(transform.localScale.x + 1) + 1;
+                    targetVelocity.y = jumpSpeed + scaleFactor;
+                    // var jumpTarget = new Vector2(currVelocity.x,jumpSpeed);
+                    // rb.velocity = Vector2.SmoothDamp(currVelocity, jumpTarget, ref velocity, snappyness * (1f/transform.localScale.magnitude * sizeJumpInfluence));
+                    // rb.AddForce(new Vector2(0f, jumpForce * (1f/transform.localScale.magnitude * sizeJumpInfluence)));
                 }
                 
             }
+
+            rb.velocity = targetVelocity;
 
             Map.singleton.reportPlayerHeight(transform.position.y); // if this player has just reached a new high point the camera will move up
         }
@@ -56,6 +74,17 @@ namespace Code.Player
         {
             horizontalFlip *= -1;
         }
-        
+
+
+        bool OnWall(int right)
+        /*
+         * right = 1 then check right wall, right = -1 then check left
+         */
+        {
+            RaycastHit2D r = Physics2D.Raycast(transform.position, Vector2.right * right, transform.localScale.x, onlyFloor);
+            return r.distance != 0.0f;
+
+            // Debug.DrawRay(transform.position, Vector3.right * right * transform.localScale.x, Color.red, 2f);
+        }
     }
 }
