5c6facc9e6293100737672dcf74314c2373649ec
sasha <reallysasha@gmail.com>
2020-07-12 18:52:02 +0200

    flips pass to child

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -20,7 +20,7 @@ namespace Code.Player
         [HideInInspector] public bool airborn = true;
         private Vector2 velocity;  // for movement interp
         private Rigidbody2D rb;
-        private int horizontalFlip = 1;
+        [HideInInspector] public int horizontalFlip = 1;
         private Vector2 targetVelocity;
 
         // public Animator animator;
