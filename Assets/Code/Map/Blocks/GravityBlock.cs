b574ba613ac95761a14babf66a86bd94f1a98d0d
shane <shaneacton745.sa@gmail.com>
2020-07-12 18:14:25 +0200

    started level system

---
+++ Assets/Code/Map/Blocks/GravityBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Code.Player;
+using UnityEngine;
+
+public class GravityBlock : EffectBlock
+{
+    public override void start()
+    {
+        base.start();
+        allowReeffects = true;
+    }
+
+    public override void affect(PlayerController pc)
+    {
+    }
+
+    public override void unaffect(PlayerController pc)
+    {
+    }
+
+}
