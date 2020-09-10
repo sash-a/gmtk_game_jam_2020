357f3d992d0ae8136cdf3e45a18ea2db3dfdac1a
Sasha Abramowitz <reallysasha@gmail.com>
2020-09-10 18:42:41 +0200

    old stuff

---
+++ Assets/ShaderStuff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderStuff : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sr.material.SetVector("_Velocity", rb.velocity / 50);
+    }
+}
